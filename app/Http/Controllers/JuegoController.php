04b670f4c3c636c5185c76f51143bc512953818f
angelgarcisala <angelgarcisala@gmail.com>
2025-06-07 20:09:29 +0300

    Actualizado juego controller para que trabaje con s3

--- app/Http/Controllers/JuegoController.php
+++ app/Http/Controllers/JuegoController.php
@@ -14,7 +14,8 @@ class JuegoController extends Controller
 {
     use AuthorizesRequests;
 
-    public function mis_juegos() {
+    public function mis_juegos()
+    {
         $userName = Auth::user()->name;
         $juegos = Juego::with('multimedias')
                         ->where('desarrollador', $userName)
@@ -23,7 +24,8 @@ class JuegoController extends Controller
         return view('juegos.mis_juegos', compact('juegos'));
     }
 
-    public function tienda(){
+    public function tienda()
+    {
         $juegos = Juego::with('multimedias')->get();
         return view('tienda.index', compact('juegos'));
     }
@@ -48,40 +50,43 @@ class JuegoController extends Controller
         $datos['desarrollador'] = Auth::user()->name;
         $datos['editor']        = Auth::user()->name;
 
-        // El slug convierte "Mi Juego" en "mi-juego"
         $slug = Str::slug($datos['titulo']);
 
-        // Guardar ZIP en storage/app/public/juegos/{slug}/{zipName}
+        // 1) Subir el ZIP a S3
         $zipFile = $request->file('zip');
         $zipName = "{$slug}_" . time() . ".zip";
-        $zipFile->storeAs("juegos/{$slug}", $zipName, "public");
-        $datos['url_descarga'] = Storage::url("juegos/{$slug}/{$zipName}");
+        $zipPath = $zipFile->storeAs("juegos/{$slug}", $zipName, 's3');
+        $datos['url_descarga'] = Storage::disk('s3')->url($zipPath);
 
         $juego = Juego::create($datos);
 
+        // 2) Subir imágenes a S3
         if ($request->hasFile('imagenes')) {
             foreach ($request->file('imagenes') as $img) {
                 $imgName = "juego-{$juego->id}_" . Str::random(8) . "." . $img->getClientOriginalExtension();
-                $img->storeAs("images/{$slug}", $imgName, "public");
+                $imgPath = $img->storeAs("images/{$slug}", $imgName, 's3');
                 $juego->multimedias()->create([
                     'tipo' => 'imagen',
-                    'url'  => Storage::url("images/{$slug}/{$imgName}"),
+                    'url'  => Storage::disk('s3')->url($imgPath),
                 ]);
             }
         }
 
+        // 3) Subir vídeos a S3
         if ($request->hasFile('videos')) {
             foreach ($request->file('videos') as $vid) {
                 $vidName = "juego-{$juego->id}_" . Str::random(8) . "." . $vid->getClientOriginalExtension();
-                $vid->storeAs("videos/{$slug}", $vidName, "public");
+                $vidPath = $vid->storeAs("videos/{$slug}", $vidName, 's3');
                 $juego->multimedias()->create([
                     'tipo' => 'video',
-                    'url'  => Storage::url("videos/{$slug}/{$vidName}"),
+                    'url'  => Storage::disk('s3')->url($vidPath),
                 ]);
             }
         }
 
-        return redirect()->route('tienda.index')->with('mensaje', 'Juego creado con éxito.');
+        return redirect()
+            ->route('tienda.index')
+            ->with('mensaje', 'Juego creado con éxito.');
     }
 
     public function show(Juego $juego)
@@ -95,97 +100,80 @@ class JuegoController extends Controller
         return view('juegos.show', compact('juego'));
     }
 
-    public function index()
-    {
-        //
-    }
-
     public function edit(Juego $juego)
     {
         $this->authorize('update', $juego);
-
         return view('juegos.edit', compact('juego'));
     }
 
     public function update(Request $request, Juego $juego)
     {
-        // 1) Validar con las mismas reglas, pasando el ID para unique
         $request->validate($this->rules($juego->id));
 
-        // 2) Guardar el slug antiguo (en caso de que el título cambie)
         $oldSlug = Str::slug($juego->titulo);
 
-        // 3) Eliminar multimedia existente marcada:
+        // 1) Eliminar multimedia seleccionada
         if ($request->has('remove_media')) {
             foreach ($request->input('remove_media') as $mediaId) {
                 $m = Multimedia::find($mediaId);
                 if ($m && $m->juego_id == $juego->id) {
-                    // La URL se guarda como "/storage/images/{slug}/{filename}" o "/storage/videos/{slug}/{filename}"
-                    $relativePath = Str::after($m->url, '/storage/');
-                    Storage::disk('s3')->delete($relativePath);
+                    $relative = Str::after($m->url, Storage::disk('s3')->url(''));
+                    Storage::disk('s3')->delete($relative);
                     $m->delete();
                 }
             }
         }
 
-        // 4) Procesar ZIP:
-        //    a) Si marcado para eliminar:
-        if ($request->has('remove_zip')) {
-            if ($juego->url_descarga) {
-                $oldZipPath = Str::after($juego->url_descarga, '/storage/');
-                Storage::disk('s3')->delete($oldZipPath);
-            }
+        // 2) Procesar ZIP
+        if ($request->has('remove_zip') && $juego->url_descarga) {
+            $relative = Str::after($juego->url_descarga, Storage::disk('s3')->url(''));
+            Storage::disk('s3')->delete($relative);
             $juego->url_descarga = null;
         }
-        //    b) Si cargó un ZIP nuevo, reemplazar:
         if ($request->hasFile('zip')) {
-            // Borrar ZIP antiguo si no se borró en el paso anterior
             if ($juego->url_descarga) {
-                $oldZipPath = Str::after($juego->url_descarga, '/storage/');
-                Storage::disk('s3')->delete($oldZipPath);
+                $relative = Str::after($juego->url_descarga, Storage::disk('s3')->url(''));
+                Storage::disk('s3')->delete($relative);
             }
-            // El slug puede cambiar (si el usuario actualizó el título); recalculamos:
             $newSlug = Str::slug($request->input('titulo'));
             $zipFile = $request->file('zip');
             $zipName = "{$newSlug}_" . time() . ".zip";
-            $zipFile->storeAs("juegos/{$newSlug}", $zipName, "public");
-            $juego->url_descarga = Storage::url("juegos/{$newSlug}/{$zipName}");
+            $zipPath = $zipFile->storeAs("juegos/{$newSlug}", $zipName, 's3');
+            $juego->url_descarga = Storage::disk('s3')->url($zipPath);
         }
 
-        // 5) Subir nuevas imágenes (si hay)
+        // 3) Subir nuevas imágenes
         if ($request->hasFile('imagenes')) {
-            // Recalcular slug en caso de que el título haya cambiado
             $newSlug = Str::slug($request->input('titulo'));
             foreach ($request->file('imagenes') as $img) {
                 $imgName = "juego-{$juego->id}_" . Str::random(8) . "." . $img->getClientOriginalExtension();
-                $img->storeAs("images/{$newSlug}", $imgName, "public");
+                $imgPath = $img->storeAs("images/{$newSlug}", $imgName, 's3');
                 $juego->multimedias()->create([
                     'tipo' => 'imagen',
-                    'url'  => Storage::url("images/{$newSlug}/{$imgName}"),
+                    'url'  => Storage::disk('s3')->url($imgPath),
                 ]);
             }
         }
 
-        // 6) Subir nuevos videos (si hay)
+        // 4) Subir nuevos vídeos
         if ($request->hasFile('videos')) {
             $newSlug = Str::slug($request->input('titulo'));
             foreach ($request->file('videos') as $vid) {
                 $vidName = "juego-{$juego->id}_" . Str::random(8) . "." . $vid->getClientOriginalExtension();
-                $vid->storeAs("videos/{$newSlug}", $vidName, "public");
+                $vidPath = $vid->storeAs("videos/{$newSlug}", $vidName, 's3');
                 $juego->multimedias()->create([
                     'tipo' => 'video',
-                    'url'  => Storage::url("videos/{$newSlug}/{$vidName}"),
+                    'url'  => Storage::disk('s3')->url($vidPath),
                 ]);
             }
         }
 
-        // 7) Actualizar los campos del juego
+        // 5) Guardar cambios del juego
         $juego->titulo            = $request->input('titulo');
         $juego->descripcion       = $request->input('descripcion');
         $juego->fecha_lanzamiento = $request->input('fecha_lanzamiento');
         $juego->precio            = $request->input('precio');
         $juego->nombre_ejecutable = $request->input('nombre_ejecutable');
-        // Si no se subió ni eliminó ZIP, url_descarga queda intacto
         $juego->save();
 
         return redirect()
@@ -212,7 +200,7 @@ class JuegoController extends Controller
             'videos.*'          => ['nullable', 'mimetypes:video/mp4,video/x-matroska', 'max:10240'],
             'zip'               => $id 
                                    ? ['nullable', 'mimes:zip', 'max:102400'] 
-                                   : ['required',  'mimes:zip', 'max:102400'],
+                                   : ['required', 'mimes:zip', 'max:102400'],
             'remove_media'      => ['nullable', 'array'],
             'remove_media.*'    => ['integer', 'exists:multimedia,id'],
             'remove_zip'        => ['nullable', 'in:1'],
